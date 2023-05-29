e681f8828fa4fc4e7519b8389d3bef8198bb4bfe
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-05-29 13:40:44 +0200

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Maryna Bolotska on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
